e241f75aba5b980d13abbe15850f4a14a9c9df2c
Joonas Nätynki <joonas.natynki@gmail.com>
2024-02-20 02:59:33 +0200

    Continued working on stuff

--- Source/ProjectLazarus/ProjectLazarus.Build.cs
+++ Source/ProjectLazarus/ProjectLazarus.Build.cs
@@ -10,7 +10,7 @@ public class ProjectLazarus : ModuleRules
 	
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
 
-		PrivateDependencyModuleNames.AddRange(new string[] { "UE4Helpers", "GameplayTags" });
+		PrivateDependencyModuleNames.AddRange(new string[] { "UE4Helpers", "GameplayTags", "FocusComponent" });
 
 		// Uncomment if you are using Slate UI
 		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
